b11919739d3f83ccdb98ef4d98c2203a15234dc4
jake@seahag.onmicrosoft.com <jake@seahag.onmicrosoft.com>
2022-11-23 22:05:35 -0600

    fi

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-// import {Application} from './App';
 import { Application } from './components/Application.js';
 
 const app = new Application();
-
-// console.log('app', app)
-const test = 'repeat(60, 90px)'
-const digits = test.replace(/\D+/g, '')
-
-// console.warn('digits', digits)
-
-const str =
-  'Hi there, my name is repeat(61, 90px), I am [age] years old, and I work in the field of [profession].';
-
-// const matches = +str.match(/(?<=\().+?(?=\,)/)[0]
-// console.log(matches);
